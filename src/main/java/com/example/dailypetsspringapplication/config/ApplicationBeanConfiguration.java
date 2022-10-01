5eb955969ee902b581077fb04e861849cbce368f
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2022-10-01 22:53:38 +0300

    Rename folders to singular

--- src/main/java/com/example/dailypetsspringapplication/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/example/dailypetsspringapplication/config/ApplicationBeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.dailypetsspringapplication.configs;
+package com.example.dailypetsspringapplication.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/example/dailypetsspringapplication/controller/HomeController.java
+++ src/main/java/com/example/dailypetsspringapplication/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.dailypetsspringapplication.controllers;
+package com.example.dailypetsspringapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
